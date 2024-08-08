b8fa61f62e151cc5f1fe7eab4c94be71c3442bad
Karla Gonzalez <gkarla33@gmail.com>
2024-08-08 14:07:04 -0500

    connected login to user profile

--- revbay/src/App.tsx
+++ revbay/src/App.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-import Home from './components/home'
-import Login from './pages/login'
-import Signup from './pages/signup'
-import NavigationBar from './components/navigation-bar'
-
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Account from './pages/account'
 import ProductsHome from './pages/products-home'
 import GetCartById from './components/cart/cart'
-
+import Home from './components/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import NavigationBar from './components/navigation-bar'
 
 export default function App() {
   return (
