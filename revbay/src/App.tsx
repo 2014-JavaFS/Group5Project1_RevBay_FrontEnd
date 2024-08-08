7562158ac802ff95c3f1daa9a2cbdd0b67d64e2a
JacobMRyan12 <jacob.martin.ryan.1.2@gmail.com>
2024-08-08 15:37:43 -0500

    Staging Current Work

--- revbay/src/App.tsx
+++ revbay/src/App.tsx
@@ -4,6 +4,8 @@ import Home from './components/home'
 import Login from './pages/login'
 import Signup from './pages/signup'
 import NavigationBar from './components/navigation-bar'
+import TransactionHistory from './components/transaction/getTransactionHistory'
+import CheckoutCart from './components/transaction/postCheckoutCart'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,6 +21,8 @@ export default function App() {
           <Route path="user/:userId" element={<Account />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
+          <Route path="transaction/History" element ={<TransactionHistory />} />
+          <Route path="transaction/Checkout"element={<CheckoutCart />} />
         </Routes> 
       </BrowserRouter>
     </>
