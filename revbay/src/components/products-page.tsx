da5217fd99775896c67fbde7e9d23de24a3b581e
Asaf Ahmed <asafahmed@Asafs-Mac-mini.local>
2024-08-08 10:54:54 -0500

    functional products page

--- revbay/src/components/products-page.tsx
+++ revbay/src/components/products-page.tsx
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { revbayServer } from "../common/revbay-server";
 export default function ProductPage(){
 
     const[products, setProducts] = useState([])
-    const[cart,setCart] = useState([])
+    const quanityRef = useRef([])
+    
         async function fetchData() {
           try {
             const response = await revbayServer.get("products")
-            const productFound = response.data.map((x: any) => ({ name: x.name, price: x.price, quantity : x.quantity }));
+            const productFound = response.data.map((x: any) => ({ id:x.productId,name: x.name, price: x.price, quantity : x.quantity }));
             
             setProducts(productFound)
           } catch (error:any) {
             console.log(error)
           } 
+        }
+
+         async function handleAddToCart (product, index){
+            const quantity = quanityRef.current[index].value
+            console.log(`Adding ${quantity} of ${product.name} ${product.id} to cart`) 
+            
         }
         
     
@@ -23,7 +30,15 @@ export default function ProductPage(){
     <ul>
         {products.length > 0 ? (
             products.map((product, index) => (
-                <li key={index}> {product.name} - ${product.price} -Qt{product.quantity}<button>Add to cart</button> </li>
+                <li key={index}> {product.name} - ${product.price} -Qt{product.quantity} <input
+                type="number"
+                min="1"
+                ref = {el=> quanityRef.current[index]=el}
+                step={1.0}
+            />
+            <button  onClick={()=>handleAddToCart(product,index)}>
+                Add to cart
+            </button> </li>
             ))
         ) : (
             <p>No Products to Display</p>
